1ea7e60bb8defd7de1fa47c5526bd3fa2510dd36
Sofa <philip.davies199127@gmail.com>
2018-10-06 19:33:51 -0700

    Updates

---
+++ precache-manifest.f7a1764cb8a54377c5e8a9db260e24e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "cb94011908825524147d",
+    "url": "/clicky-game/static/js/main.cb940119.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/clicky-game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "cb94011908825524147d",
+    "url": "/clicky-game/static/css/main.3e57bfd9.chunk.css"
+  },
+  {
+    "revision": "4b7ac9d4668aecd60ed33168ff1c195a",
+    "url": "/clicky-game/index.html"
+  }
+];
